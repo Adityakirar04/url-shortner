b0b0a7f71b8a821751c849c53383975a760a414d
Adityakirar04 <mr.kirar62@gmail.com>
2026-06-14 18:53:09 +0530

    Initial URL Shortener Project

---
+++ index.js
@@ -0,0 +1,23 @@
+ const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+
+const app = express();
+
+connectToMongoDB(
+  "mongodb://127.0.0.1:27017/short-url"
+)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.use("/url", urlRoute);
+
+app.listen(8001, () => {
+  console.log("Server Started at PORT 8001");
+});
